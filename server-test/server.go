aa43ec8f8afb034da0864dcfb16bc7b5b6a4face
schleising <stephen@schleising.net>
2024-03-24 22:07:24 +0000

    Update match score display in template.html and import necessary packages in server.go

--- server-test/server.go
+++ server-test/server.go
@@ -2,58 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"html/template"
-	"io"
 	"net/http"
-	"strings"
-	"time"
+
+	"mongodb-test"
+	"mongodb-test/models"
 )
-
-// Create a Match struct
-type Match struct {
-	Status        string    `json:"status"`
-	StartTime     time.Time `json:"start_time_iso"`
-	HomeTeam      string    `json:"home_team"`
-	HomeTeamScore int       `json:"home_team_score"`
-	AwayTeam      string    `json:"away_team"`
-	AwayTeamScore int       `json:"away_team_score"`
-}
-
-// Add a String method to the Match struct
-func (m Match) String() string {
-	return fmt.Sprintf(
-		"%s - %-15s %2d - %-2d %-15s %-12s",
-		m.StartTime.Local().Format(time.RFC1123),
-		m.HomeTeam,
-		m.HomeTeamScore,
-		m.AwayTeamScore,
-		m.AwayTeam,
-		m.Status,
-	)
-}
-
-// Create a Matches struct
-type Matches struct {
-	Matches []Match `json:"matches"`
-}
-
-// Add a String method to the Matches struct
-func (m Matches) String() string {
-	var str string
-
-	// Loop through the matches and add them to the string
-	for _, match := range m.Matches {
-		str += match.String() + "\n"
-	}
-
-	// Trim the string
-	str = strings.TrimSpace(str)
-
-	// Return the string
-	return str
-}
 
 type ctxKeys string
 
@@ -76,11 +31,6 @@ func main() {
 
 	// Wrap the handlers with the logHandler function
 	handler := logHandler(mux)
-
-	// Redirect all requests without a trailing slash to one with a trailing slash
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//     http.Redirect(w, r, r.URL.Path+"/", http.StatusMovedPermanently)
-	// })
 
 	// Listen and serve on port 8080
 	http.ListenAndServe(":8080", handler)
@@ -176,38 +126,26 @@ func apiHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func getMatches() (Matches, error) {
-	// Matches struct
-	var matches Matches
-
-	// Get data from schleising.net/football/api/
-	resp, err := http.Get("https://www.schleising.net/football/api/")
+func getMatches() (models.MatchList, error) {
+	// Create a new MongoDB test
+	mongo, err := mongodb_test.NewMongoTest()
 
 	// Check for errors
 	if err != nil {
-		return matches, err
+		return models.MatchList{}, err
 	}
+	
+	// Defer the close
+	defer mongo.Close()
 
-	// Check the status code
-	if resp.StatusCode != 200 {
-		return matches, err
-	}
-
-	// Defer the closing of the response body
-	defer resp.Body.Close()
-
-	// Read the response body
-	body, err := io.ReadAll(resp.Body)
+	// Get the matches
+	matches, err := mongo.GetAllTeamMatches("Liverpool")
 
 	// Check for errors
 	if err != nil {
-		return matches, err
+		return models.MatchList{}, err
 	}
 
-	// Unmarshal the response body into a Matches struct
-	if err := json.Unmarshal(body, &matches); err != nil {
-		return matches, err
-	}
-
+	// Return the matches
 	return matches, nil
 }
